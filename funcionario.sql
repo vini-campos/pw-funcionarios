322436e00dfb314cc9dd8a189c30cd969237529d
vini-campos <viniciuscampos131209@gmail.com>
2026-06-11 20:40:59 -0300

    feat(sql): Adição do esquema da tabela funcionarios

---
+++ funcionario.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Funcionario
+USE Funcionario
+
+CREATE TABLE Funcionario(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50),
+    Endereco VARCHAR(50),
+    Idade INT,
+    DataNasc DATE,
+    Foto VARCHAR(50)
+);
