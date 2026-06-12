f539097f53586d1fc2d8370deb2836b2fccf7b3f
vini-campos <viniciuscampos131209@gmail.com>
2026-06-12 19:05:38 -0300

    style(code): Formatação

--- modalDel.php
+++ modalDel.php
@@ -1,21 +1,15 @@
 <div class="modal fade" id="modalExcluir" tabindex="-1">
-
     <div class="modal-dialog">
-
         <div class="modal-content">
-
             <div class="modal-header">
                 <h5 class="modal-title">
                     Confirmar exclusão
                 </h5>
             </div>
-
             <div class="modal-body">
                 Deseja realmente excluir?
             </div>
-
             <div class="modal-footer">
-
                 <button class="btn btn-secondary" data-bs-dismiss="modal">
                     Cancelar
                 </button>
@@ -23,11 +17,7 @@
                 <a id="btnExcluir" href="#" class="btn btn-danger">
                     Excluir
                 </a>
-
             </div>
-
         </div>
-
     </div>
-
 </div>
